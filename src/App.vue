76d3b0165f2e91af404301b196d43c78edca4954
張文相 Zhang Wenxiang (旋風之音 GoneTone) <p29022716@gmail.com>
2021-03-07 20:46:10 +0800

    更換背景音樂

--- src/App.vue
+++ src/App.vue
@@ -23,22 +23,12 @@ export default {
   mounted () {
     this.$store.dispatch('setDatas')
 
-    const audios = [
-      'Venti Chinese VA (喵☆酱) - Patchwork Staccato (ツギハギスタッカート) (Tsugihagi Staccato) Lyrics KAN_ROM_ENG.mp3',
-      'Xiao\'s VA Sings - Vigilant Yaksha ft. @kin sen  (Official Lyrics Video) _ Prod. by MiXiao.mp3',
-      '【Original Genshin Fansong】让风告诉你 (Let the Wind Tell You).mp3',
-      'Genshin Impact Fansong - Hutao【胡口桃生】.mp3'
-    ]
-    audios.sort(function () {
-      return (0.5 - Math.random())
-    })
-
     this.$nextTick(function () {
       (function ($) {
         const bapRender = $('#bapRender')
         bapRender.buttonAudioPlayer({
           type: 'default',
-          src: require(`@/assets/audio/background/${audios[Math.floor(Math.random() * audios.length)]}`)
+          src: require('@/assets/audio/background/Hu Tao Theme Music EXTENDED - Let the Living Beware (tnbee mix) _ Genshin Impact.mp3')
         })
         bapRender.find('button').click()
         // eslint-disable-next-line no-undef
