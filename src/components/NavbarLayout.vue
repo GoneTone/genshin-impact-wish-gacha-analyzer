68e08c318e94c5ad25a1b7b2333fb307121ffceb
張文相 Zhang Wenxiang (旋風之音 GoneTone) <p29022716@gmail.com>
2021-03-09 22:23:09 +0800

    側選單開關音效

--- src/components/NavbarLayout.vue
+++ src/components/NavbarLayout.vue
@@ -114,7 +114,7 @@
     <hr class="sidebar-divider d-none d-md-block">
 
     <div class="text-center d-none d-md-inline">
-      <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      <button class="rounded-circle border-0" id="sidebarToggle" @click="this.$store.dispatch('playerAudioEffect', 'switch_task')"></button>
     </div>
   </ul>
 </template>
