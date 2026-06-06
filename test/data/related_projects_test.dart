9800889448b50a537bb56a71865f8de1b279c0c8
GoneTone <p29022716@gmail.com>
2026-06-07 02:19:20 +0800

    test(data): assert RelatedProjects URL structure, align with sibling tests

--- test/data/related_projects_test.dart
+++ test/data/related_projects_test.dart
@@ -3,13 +3,19 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:genshin_impact_wish_gacha_analyzer/data/related_projects.dart';
 
 void main() {
-  test(
-    'wutheringWavesAnalyzer points to the WW convene gacha analyzer repo',
-    () {
+  group('RelatedProjects', () {
+    test('wutheringWavesAnalyzer is a well-formed HTTPS GitHub URL', () {
+      final uri = Uri.parse(RelatedProjects.wutheringWavesAnalyzer);
+      expect(uri.scheme, 'https');
+      expect(uri.host, 'github.com');
+      expect(uri.pathSegments, isNotEmpty);
+    });
+
+    test('wutheringWavesAnalyzer points to the GoneTone WW analyzer repo', () {
       expect(
         RelatedProjects.wutheringWavesAnalyzer,
-        'https://github.com/GoneTone/wuthering-waves-convene-gacha-analyzer',
+        contains('/GoneTone/wuthering-waves-convene-gacha-analyzer'),
       );
-    },
-  );
+    });
+  });
 }
